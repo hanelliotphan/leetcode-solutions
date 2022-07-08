e8fd616af179b8d208de2764f64677485a794d01
han <hannguyendev@gmail.com>
2022-07-08 15:30:52 -0700

    Create 2235-AddTwoIntegers.py

---
+++ 2235-AddTwoIntegers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        """
+        Logic: Straightforward
+        
+        Time: O(1)
+        Space: O(1)
+        """
+        return num1 + num2
