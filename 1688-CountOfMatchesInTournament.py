5e25c4e412ffe0b130465fd35776d54392a93e05
han <hannguyendev@gmail.com>
2022-07-09 19:12:03 -0700

    Create 1688-CountOfMatchesInTournament.py

---
+++ 1688-CountOfMatchesInTournament.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        """
+        Logic: Mathematical Logic
+        
+        Time: O(1)
+        Space: O(1)
+        """
+        return n - 1
