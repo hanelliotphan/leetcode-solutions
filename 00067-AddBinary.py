b1664508f768d65b043776b20bcf44dcf590c789
han <hannguyendev@gmail.com>
2022-07-10 17:10:13 -0700

    Create 00067-AddBinary.py

---
+++ 00067-AddBinary.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        """
+        Logic: Binary Format
+        
+        Time: O(1)
+        Space: O(1)
+        """
+        return "{0:b}".format(int(a, 2) + int(b, 2))
