cde28bbdcc1d0ea39b6bdb8232c874a259b93b49
han <hannguyendev@gmail.com>
2022-08-31 20:27:10 -0700

    Create 01025-DivisorGame.py

---
+++ 01025-DivisorGame.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        """
+        Logic: Mathematical approach
+        
+        Time: O(1)
+        Space: O(1)
+        """
+        return True if n % 2 == 0 else False
