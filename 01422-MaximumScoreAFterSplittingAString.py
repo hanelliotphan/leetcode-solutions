2dfb48d8b109a872b6d6a04f7e40bc5d51d3d7f5
han <hannguyendev@gmail.com>
2022-08-31 20:11:13 -0700

    Create 01422-MaximumScoreAFterSplittingAString.py

---
+++ 01422-MaximumScoreAFterSplittingAString.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        """
+        Logic: Counting
+        
+        Time: O(n)
+        Space: O(1)
+        """
+        max_score = 0
+        
+        for i in range(1, len(s)):
+            left = s[:i].count("0")
+            right = s[i:].count("1")
+            max_score = max(max_score, left + right)
+            
+        return max_score
