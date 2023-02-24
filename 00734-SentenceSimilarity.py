7095809f82acc4d4d58e1aae8e18c49c4010fe1a
han <hannguyendev@gmail.com>
2023-02-24 11:22:15 -0800

    Create 00734-SentenceSimilarity.py

---
+++ 00734-SentenceSimilarity.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sentence-similarity/description/
+
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        """
+        Logic: Brute Force
+
+        Time: O(n)
+        Space: O(1)
+        """
+        if len(sentence1) != len(sentence2):
+            return False
+
+        for i in range(len(sentence1)):
+            if not(sentence1[i] == sentence2[i] or [sentence1[i], sentence2[i]] in similarPairs or [sentence2[i], sentence1[i]] in similarPairs):
+                return False
+
+        return True
