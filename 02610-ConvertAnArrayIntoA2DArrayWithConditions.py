1c7b199bc3da2be855773091ae6273cd006891fe
han <hannguyendev@gmail.com>
2023-04-07 21:19:19 -0700

    Create 02610-ConvertAnArrayIntoA2DArrayWithConditions.py

---
+++ 02610-ConvertAnArrayIntoA2DArrayWithConditions.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/description/
+
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        """
+        Logic: Hash Map
+
+        Time: O(n * max(counter.values()))
+        Space: O(1) --> need to return 2D array, no extra space needed
+        """
+        counter = collections.Counter(nums)
+        res = [[0] for _ in range(max(list(counter.values())))]
+
+        for i in range(len(nums)):
+            j = 0
+            while counter[nums[i]] > 0:
+                res[j].append(nums[i])
+                j += 1
+                counter[nums[i]] -= 1
+        
+        return [r[1:] for r in res]
