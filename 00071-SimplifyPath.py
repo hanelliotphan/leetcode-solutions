50ab46a1c1da8b7b39f6cccee66983d8226b601e
han <hannguyendev@gmail.com>
2023-04-11 18:50:22 -0700

    Update 00071-SimplifyPath.py

--- 00071-SimplifyPath.py
+++ 00071-SimplifyPath.py
@@ -4,18 +4,19 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         """
         Logic: Stack
-        
+
         Time: O(n)
         Space: O(n)
         """
+        path_components = path.split("/")
         stack = []
-        
-        for path_component in path.split("/"):
-            if path_component == "..":
-                if stack: stack.pop()
-            elif path_component == "." or not path_component:
+
+        for comp in path_components:
+            if not comp or comp == ".":
                 continue
+            elif comp == "..":
+                if stack: stack.pop()
             else:
-                stack.append(path_component)
-                
+                stack.append(comp)
+        
         return "/" + "/".join(stack)
