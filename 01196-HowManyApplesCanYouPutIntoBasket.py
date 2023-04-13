e919eedc73eda400c056bcd77aedab015d5c0cf4
han <hannguyendev@gmail.com>
2023-04-12 22:09:02 -0700

    Update 01196-HowManyApplesCanYouPutIntoBasket.py

--- 01196-HowManyApplesCanYouPutIntoBasket.py
+++ 01196-HowManyApplesCanYouPutIntoBasket.py
@@ -1,19 +1,23 @@
 class Solution:
     def maxNumberOfApples(self, weight: List[int]) -> int:
         """
-        Logic: Sort
+        Logic: Counting Sort
 
-        Time: O(n*logn)
-        Space: O(1)
+        Time: O(n)
+        Space: O(n)
         """
-        weight.sort()
-        res = count = 0
+        count = [0]*1001
+        curr_weight = apples = 0
 
         for w in weight:
-            if res >= 5000:
-                break
-            else:
-                res += w
-                count += 1
+            count[w] += 1
         
-        return count if res <= 5000 else count-1
+        for i in range(1, 1001):
+            while curr_weight + i <= 5000 and count[i] > 0:
+                count[i] -= 1
+                curr_weight += i
+                apples += 1
+            if curr_weight + i > 5000:
+                break
+        
+        return apples
