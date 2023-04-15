c5ac70dfdf78f9cb62cd92fa38f228d71096eaec
han <hannguyendev@gmail.com>
2023-04-14 17:47:04 -0700

    Update 00003-LongestSubstringWithoutRepeatingCharacters.py

--- 00003-LongestSubstringWithoutRepeatingCharacters.py
+++ 00003-LongestSubstringWithoutRepeatingCharacters.py
@@ -3,24 +3,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        Logic: Hashmap of visited indices
-            - If character already visited and current index
-        shows that the character is just new, jump to next char
-            - Else, update max_length of all the characters
-        just visited
-            - Then, update index of visited character
+        Logic: Hash Map
         
         Time: O(n)
         Space: O(n)
         """
-        start_idx, max_len = 0, 0
-        visited = {}
-        
+        hm = dict()
+        l = 0
+        longest = float("-inf")
+
         for i in range(len(s)):
-            if s[i] in visited and start_idx <= visited[s[i]]:
-                start_idx = visited[s[i]] + 1
-            else:
-                max_len = max(max_len, i-start_idx+1)
-            visited[s[i]] = i
-            
-        return max_len
+            if s[i] in hm and hm[s[i]] >= l:
+                l = hm[s[i]] + 1
+            hm[s[i]] = i
+            longest = max(longest, i-l+1)
+        
+        return longest if longest != float("-inf") else 0
