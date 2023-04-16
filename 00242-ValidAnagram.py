c55e6d751996df23bd20728a3082fdbea79f7b40
han <hannguyendev@gmail.com>
2023-04-16 14:05:49 -0700

    Update 00242-ValidAnagram.py

--- 00242-ValidAnagram.py
+++ 00242-ValidAnagram.py
@@ -1,25 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         """
-        Logic: Hash Map
-            - Store the occurrences of characters in `s` and `t`
-            - in a hash map then compare the value at the character key.
-            - If equal, return True, if not equal return False
-            
+        Logic: Hash Map/Counter
+        
         Time: O(n)
         Space: O(n)
         """        
-        if len(s) != len(t):
-            return False
-        
-        hash_s, hash_t = {}, {}
-        
-        for i in range(len(s)):
-            hash_s[s[i]] = hash_s.get(s[i], 0) + 1
-            hash_t[t[i]] = hash_t.get(t[i], 0) + 1
-            
-        for c in hash_s:
-            if hash_s[c] != hash_t.get(c):
+        cs = collections.Counter(s)
+
+        for ch in t:
+            if ch not in cs:
                 return False
-            
+            cs[ch] -= 1
+
+        for v in cs.values():
+            if v != 0:
+                return False
+        
         return True
