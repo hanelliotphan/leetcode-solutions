08c4aa9ee691bd707d8e5249b74c465e036d4d70
han <hannguyendev@gmail.com>
2023-04-16 22:38:43 -0700

    Create 00704-BinarySearch.py

---
+++ 00704-BinarySearch.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/binary-search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        Logic: Binary Search
+        
+        Time: O(logn)
+        Space: O(1)
+        """
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return -1
