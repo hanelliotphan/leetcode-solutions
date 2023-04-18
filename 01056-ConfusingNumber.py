6fadc17dc11c9eba668e36b4bc87c1bcabe912cb
han <hannguyendev@gmail.com>
2023-04-17 19:15:03 -0700

    Update 01056-ConfusingNumber.py

--- 01056-ConfusingNumber.py
+++ 01056-ConfusingNumber.py
@@ -8,35 +8,20 @@ class Solution:
         Time: O(d)
         Space: O(d) -- d = number of digits of `n`
         """
-        confusing_digits = {
-            0: 0,
-            1: 1,
-            6: 9,
-            8: 8,
-            9: 6
+        if n == 0: return False
+
+        confusing_nums = {
+            0: 0, 1: 1, 6: 9, 8: 8, 9: 6
         }
-        
+        n_copy = n
         digits = []
-        tmp = n
-        new_n = ""
-        
-        if n == 0 or n == 1 or n == 8:
-            return False
-        elif n == 6 or n == 9:
-            return True
-        
-        while tmp > 0:
-            d = tmp % 10
-            if d not in confusing_digits.keys():
+
+        while n_copy > 0:
+            d = n_copy % 10
+            if d not in confusing_nums:
                 return False
-            digits.append(d)
-            new_n += str(confusing_digits[d])
-            tmp //= 10
-            
-        if len(set(digits)) == 1:
-            return False
-        
-        if int(new_n) == n:
-            return False
-                
-        return True
+            else:
+                digits.append(str(confusing_nums[d]))
+            n_copy //= 10
+
+        return int("".join(digits)) != n
