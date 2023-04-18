3524a1a57c4df0157e97d9b258227782800eefad
han <hannguyendev@gmail.com>
2023-04-17 22:49:19 -0700

    Update 00944-DeleteColumnsToMakeSorted.py

--- 00944-DeleteColumnsToMakeSorted.py
+++ 00944-DeleteColumnsToMakeSorted.py
@@ -3,18 +3,19 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
         """
-        Logic: ASCII 
-
-        Time: O(n^2)
+        Logic: Brute Force with ASCII
+        
+        Time: O(n*m)
         Space: O(1)
         """
-        r, c = len(strs), len(strs[0])
-        num_cols_to_delete = 0
+        r = len(strs)
+        c = len(strs[0])
+        count = 0
 
         for j in range(c):
             for i in range(1, r):
                 if ord(strs[i][j]) < ord(strs[i-1][j]):
-                    num_cols_to_delete += 1
+                    count += 1
                     break
         
-        return num_cols_to_delete
+        return count
