e1f33dcfceacca76c50541449e84046267a5c379
han <hannguyendev@gmail.com>
2023-04-18 18:37:46 -0700

    Update 01528-ShuffleString.py

--- 01528-ShuffleString.py
+++ 01528-ShuffleString.py
@@ -1,14 +1,18 @@
+# https://leetcode.com/problems/shuffle-string/description/
+
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         """
-        Logic: Placeholder array
+        Logic: Brute Force
         
         Time: O(n)
         Space: O(n)
         """
-        res = [''] * len(s)
+        res = [""] * len(s)
+        j = 0
+
+        for i in indices:
+            res[i] = s[j]
+            j += 1
         
-        for i, c in enumerate(s):
-            res[indices[i]] = c
-            
         return "".join(res)
