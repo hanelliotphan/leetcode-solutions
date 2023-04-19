a4e01ab7cb9f89c41db7ec02acc802c37a553ed3
han <hannguyendev@gmail.com>
2023-04-18 22:21:31 -0700

    Update 01903-LargestOddNumberInString.py

--- 01903-LargestOddNumberInString.py
+++ 01903-LargestOddNumberInString.py
@@ -3,16 +3,13 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
         """
-        Logic: Brute Force
-
+        Logic: Brute Force (find the rightmost odd digit)
+        
         Time: O(n)
         Space: O(1)
         """
-        n = len(num)
-        res = ""
-        
-        for i in range(n-1, -1, -1):
-            if int(num[i]) % 2 == 1:
+        for i in range(len(num)-1, -1, -1):
+            if int(num[i]) % 2:
                 return num[:i+1]
         
-        return res
+        return ""
