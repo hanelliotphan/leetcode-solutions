d5858ecbfad52a57019dd75ea2879e5ab2301a01
han <hannguyendev@gmail.com>
2023-04-18 19:14:37 -0700

    Update 01704-DetermineIfStringHalvesAreAlike.py

--- 01704-DetermineIfStringHalvesAreAlike.py
+++ 01704-DetermineIfStringHalvesAreAlike.py
@@ -1,19 +1,21 @@
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
+
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         """
         Logic: Brute Force
         
         Time: O(n)
-        Space: O(n)
+        Space: O(1)
         """
-        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        fm = sm = 0
         mid = len(s) // 2
+        c1 = c2 = 0
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+        for ch in s[:mid]:
+            if ch in vowels: c1 += 1
+
+        for ch in s[mid:]:
+            if ch in vowels: c2 += 1
         
-        for i in range(len(s)):
-            if i < mid and s[i] in vowels:
-                fm += 1
-            elif i >= mid and s[i] in vowels:
-                sm += 1
-        
-        return True if fm == sm else False
+        return c1 == c2
