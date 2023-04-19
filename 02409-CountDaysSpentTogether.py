05b2cef92b9c2dd75d6c037a0eb74730a87c616c
han <hannguyendev@gmail.com>
2023-04-18 17:19:57 -0700

    Update 02409-CountDaysSpentTogether.py

--- 02409-CountDaysSpentTogether.py
+++ 02409-CountDaysSpentTogether.py
@@ -8,22 +8,28 @@ class Solution:
         Time: O(1) --> loop through 12 months
         Space: O(1) --> 12-month size hash map
         """
-        month_days_map = {
-            1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
-            7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
+        md_map = {
+            1: 31, 2: 28, 3: 31, 4: 30,
+            5: 31, 6: 30, 7: 31, 8: 31,
+            9: 30, 10: 31, 11: 30, 12: 31
         }
-        
+
         amA, adA = arriveAlice.split("-")
+        num_arrive_A = sum([md_map[i] for i in range(1, int(amA))]) + int(adA)
+
         lmA, ldA = leaveAlice.split("-")
+        num_leave_A = sum([md_map[i] for i in range(1, int(lmA))]) + int(ldA)
+
         amB, adB = arriveBob.split("-")
+        num_arrive_B = sum([md_map[i] for i in range(1, int(amB))]) + int(adB)
+
         lmB, ldB = leaveBob.split("-")
-        
-        num_days_arrive_Alice = sum([month_days_map[i] for i in range(1, int(amA))]) + int(adA)
-        num_days_leave_Alice = sum([month_days_map[i] for i in range(1, int(lmA))]) + int(ldA)
-        nums_day_arrive_Bob = sum([month_days_map[i] for i in range(1, int(amB))]) + int(adB)
-        nums_day_leave_Bob = sum([month_days_map[i] for i in range(1, int(lmB))]) + int(ldB)
-                
-        arrive_time = max(num_days_arrive_Alice, nums_day_arrive_Bob)
-        leave_time = min(num_days_leave_Alice, nums_day_leave_Bob)
-        
-        return leave_time - arrive_time + 1 if leave_time >= arrive_time else 0
+        num_leave_B = sum([md_map[i] for i in range(1, int(lmB))]) + int(ldB)
+
+        arrive_day = max(num_arrive_A, num_arrive_B)
+        leave_day = min(num_leave_A, num_leave_B)
+
+        if arrive_day > leave_day:
+            return 0
+
+        return leave_day - arrive_day + 1
