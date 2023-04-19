eb31e8bef86979ec018456016a48bc61ed44f086
han <hannguyendev@gmail.com>
2023-04-18 21:51:57 -0700

    Update 02053-KthDistinctStringInAnArray.py

--- 02053-KthDistinctStringInAnArray.py
+++ 02053-KthDistinctStringInAnArray.py
@@ -1,16 +1,19 @@
+# https://leetcode.com/problems/kth-distinct-string-in-an-array/description/
+
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         """
-        Logic: Counter
+        Logic: Hash Map/Counter
         
         Time: O(n)
         Space: O(n)
         """
         counter = collections.Counter(arr)
-        
-        for key, val in counter.items():
-            if val == 1:
+
+        for n, freq in counter.items():
+            if freq == 1:
                 k -= 1
-                if k == 0: return key
+            if not k:
+                return n
         
         return ""
