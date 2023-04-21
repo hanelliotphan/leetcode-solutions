758292c5ad8b677b20ea75212ea19c7941d55e0e
han <hannguyendev@gmail.com>
2023-04-20 22:35:41 -0700

    Create 00709-ToLowerCase.py

---
+++ 00709-ToLowerCase.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        """
+        Logic: Brute Force
+        
+        Time: O(n)
+        Space: O(1)
+        """
+        return s.lower()
