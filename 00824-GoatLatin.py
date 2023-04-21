e45818b9e8fa70ae342bb848297d5637a59cd96a
han <hannguyendev@gmail.com>
2023-04-20 23:01:18 -0700

    Update 00824-GoatLatin.py

--- 00824-GoatLatin.py
+++ 00824-GoatLatin.py
@@ -1,19 +1,22 @@
+# https://leetcode.com/problems/goat-latin/description/
+
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
         """
         Logic: Brute Force
         
-        Time: O(w)
-        Space: O(w)
+        Time: O(n)
+        Space: O(n)
         """
-        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         words = sentence.split()
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+        for i, word in enumerate(words):
+            if word in vowels:
+                words[i] += ("ma" + "a"*(i+1))
+            elif word[0] not in vowels:
+                words[i] = (word[1:]+word[0]+"ma"+"a"*(i+1))
+            else:
+                words[i] = word+"ma"+"a"*(i+1)
         
-        for i in range(len(words)):
-            if words[i][0] not in vowels:
-                new_word = words[i][1:] + words[i][0]
-                words[i] = new_word
-            words[i] += "ma"
-            words[i] += "a"*(i+1)
-            
         return " ".join(words)
