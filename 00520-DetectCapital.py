13556f632a35f8d125204a96978fac1298b4e69e
han <hannguyendev@gmail.com>
2023-04-21 17:58:08 -0700

    Update 00520-DetectCapital.py

--- 00520-DetectCapital.py
+++ 00520-DetectCapital.py
@@ -8,21 +8,13 @@ class Solution:
         Time: O(n)
         Space: O(1)
         """
-        first_upper = False
-        mid_upper = False
-        len_upper = 0
-        
-        for i in range(len(word)):
-            if word[i].isupper():
-                len_upper += 1
-            if word[i].isupper() and i == 0:
-                first_upper = True
-            if word[i].isupper() and i != 0:
-                mid_upper = True
-                
-        if len_upper == len(word) or len_upper == 0:
-            return True
-        if first_upper == True and mid_upper == False:
-            return True
-        
-        return False
+        c1, c2, c3 = False, False, False
+
+        if word == word.upper():
+            c1 = True
+        if word == word.lower():
+            c2 = True
+        if word[0] == word[0].upper() and word[1:] == word[1:].lower():
+            c3 = True
+
+        return c1 or c2 or c3
