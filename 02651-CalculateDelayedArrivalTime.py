f3c4492e11c722106dd18e0ed529c09bb31142a8
han <hannguyendev@gmail.com>
2023-04-23 22:40:02 -0700

    Create 02651-CalculateDelayedArrivalTime.py

---
+++ 02651-CalculateDelayedArrivalTime.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
+        """
+        Logic: Brute Force
+        
+        Time: O(1)
+        Space: O(1)
+        """
+        if arrivalTime + delayedTime >= 24:
+            return arrivalTime + delayedTime - 24
+        else:
+            return arrivalTime + delayedTime
