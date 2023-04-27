9d7c47c0b4261c428ee61597d554edf0b1a53c21
han <hannguyendev@gmail.com>
2023-04-26 22:26:09 -0700

    Create 00319-BulbSwitcher.py

---
+++ 00319-BulbSwitcher.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/bulb-switcher/
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        """
+        Logic: Mathematical Approach
+
+        Time: O(1)
+        Sapce: O(1)
+        """
+        return int(sqrt(n))
