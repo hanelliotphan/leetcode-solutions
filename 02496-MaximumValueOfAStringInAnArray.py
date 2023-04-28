383264da5c394cf20c593853cc8e995055ac852c
han <hannguyendev@gmail.com>
2023-04-28 14:57:08 -0700

    Update 02496-MaximumValueOfAStringInAnArray.py

--- 02496-MaximumValueOfAStringInAnArray.py
+++ 02496-MaximumValueOfAStringInAnArray.py
@@ -1,21 +1,19 @@
+# https://leetcode.com/problems/maximum-value-of-a-string-in-an-array/description/
+
 class Solution:
     def maximumValue(self, strs: List[str]) -> int:
         """
         Logic: Brute Force
-
+        
         Time: O(n)
         Space: O(1)
         """
-        max_val = 0
-        
-        for s in strs:
-            is_all_digit = True
-            for ch in s:
-                if not (48 <= ord(ch) <= 57):
-                    max_val = max(max_val, len(s))
-                    is_all_digit = False
-                    break
-            if is_all_digit:
-                max_val = max(max_val, int(s))
+        max_val = 0 
 
+        for s in strs:
+            if s.isnumeric():
+                max_val = max(max_val, int(s))
+            else:
+                max_val = max(max_val, len(s))
+        
         return max_val
