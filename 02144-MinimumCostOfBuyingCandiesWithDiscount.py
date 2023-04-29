c3bc99ffc95e2c3d473790f2ca66800872e97661
han <hannguyendev@gmail.com>
2023-04-29 12:48:01 -0700

    Update 02144-MinimumCostOfBuyingCandiesWithDiscount.py

--- 02144-MinimumCostOfBuyingCandiesWithDiscount.py
+++ 02144-MinimumCostOfBuyingCandiesWithDiscount.py
@@ -1,18 +1,22 @@
-# https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/
+# https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/description/
 
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
         """
-        Logic: Sort
+        Logic: Brute Force with Sort
         
         Time: O(n*logn)
         Space: O(1)
         """
-        cost = sorted(cost, reverse=True)
-        total, i, n = 0, 0, len(cost)
+        cost.sort(reverse=True)
+        total = 0
+        i = 0
+
+        for c in cost:
+            if i == 2:
+                i = 0
+                continue
+            total += c
+            i += 1
         
-        while i < n:
-            total += sum(cost[i:i+2])
-            i += 3
-            
         return total
