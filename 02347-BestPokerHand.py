164a4d19564dee1473b9826cce70b9a5161ba33a
han <hannguyendev@gmail.com>
2023-05-05 23:34:40 -0700

    Update 02347-BestPokerHand.py

--- 02347-BestPokerHand.py
+++ 02347-BestPokerHand.py
@@ -1,26 +1,28 @@
+# https://leetcode.com/problems/best-poker-hand/description/
+
 class Solution:
     def bestHand(self, ranks: List[int], suits: List[str]) -> str:
         """
-        Logic: Hash Set / Counter
+        Logic: Hash Map/Counter
         
-        Time: O(n)
-        Space: O(n)
+        Time: O(r+s)
+        Space: O(r+s)
         """
-        poker_rank = {3: 3, 2: 2, 1: 1}
-        poker_suit = {5: 4}
-        rank_points = collections.Counter(ranks)
-        suit_points = collections.Counter(suits)
-        max_point = 1
+        rank_counter = collections.Counter(ranks)
+        suit_counter = collections.Counter(suits)
+        curr_best_rank = curr_best_suit = 1
+
+        for k, v in rank_counter.items():
+            curr_best_rank = max(curr_best_rank, v)
+
+        for k, v in suit_counter.items():
+            curr_best_suit = max(curr_best_suit, v)
         
-        for point in rank_points.values():
-            if point > 3: point = 3
-            max_point = max(max_point, poker_rank[point])
-            
-        for point in suit_points.values():
-            if point < 5: continue
-            max_point = max(max_point, poker_suit[point])
-                        
-        if max_point == 4: return "Flush"
-        elif max_point == 3: return "Three of a Kind"
-        elif max_point == 2: return "Pair"
-        else: return "High Card"
+        if curr_best_suit == 5:
+            return "Flush"
+        if curr_best_rank >= 3:
+            return "Three of a Kind"
+        if curr_best_rank >= 2:
+            return "Pair"
+        else:
+            return "High Card"
