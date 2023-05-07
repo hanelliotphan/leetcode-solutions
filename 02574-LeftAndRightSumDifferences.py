0f90b929582d48ccf81b542c872f56b5df16f625
han <hannguyendev@gmail.com>
2023-05-07 10:36:56 -0700

    Update 02574-LeftAndRightSumDifferences.py

--- 02574-LeftAndRightSumDifferences.py
+++ 02574-LeftAndRightSumDifferences.py
@@ -3,19 +3,24 @@
 class Solution:
     def leftRigthDifference(self, nums: List[int]) -> List[int]:
         """
-        Logic: Brute Force
-
-        Time: O(n^2) --> sum() takes O(n) time
-        Space: O(1) --> need to return an `O(n) array`, no extra data is needed
+        Logic: Prefix Sum
+        
+        Time: O(n)
+        Space: O(n)
         """
+        prefix_sum = [0] * len(nums)
+        prefix_sum[0] = nums[0]
         res = []
 
+        for i in range(1, len(nums)):
+            prefix_sum[i] = prefix_sum[i-1] + nums[i]
+        
         for i in range(len(nums)):
             if i == 0:
-                res.append(sum(nums[i+1:]))
+                res.append(abs(0-prefix_sum[i-1]+prefix_sum[i]))
             elif i == len(nums)-1:
-                res.append(sum(nums[:i]))
+                res.append(abs(prefix_sum[i-1]))
             else:
-                res.append(abs(sum(nums[:i])-sum(nums[i+1:])))
+                res.append(abs(prefix_sum[i-1]-abs(prefix_sum[i]-prefix_sum[-1])))
 
         return res
