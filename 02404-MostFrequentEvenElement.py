ceaa63b78d75b036251b62f50cf0a8c263721028
han <hannguyendev@gmail.com>
2023-05-17 18:19:50 -0700

    Update 02404-MostFrequentEvenElement.py

--- 02404-MostFrequentEvenElement.py
+++ 02404-MostFrequentEvenElement.py
@@ -1,26 +1,27 @@
-# https://leetcode.com/problems/most-frequent-even-element/
+# https://leetcode.com/problems/most-frequent-even-element/description/
 
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         """
-        Logic: Hash Map
+        Logic: Hash Map/Counter
         
         Time: O(n)
         Space: O(n)
         """
-        freqs = dict()
-        
+        even = collections.defaultdict(int)
+
         for n in nums:
-            if n % 2 == 0 and n not in freqs:
-                freqs[n] = 1
-            elif n % 2 == 0 and n in freqs:
-                freqs[n] += 1
-                    
-        curr_max_freq = float("-inf")
-        curr_min_num = float("inf")
+            if n % 2 == 0:
+                even[n] += 1
         
-        for k, v in freqs.items():
-            if v == max(list(freqs.values())):
-                curr_min_num = min(curr_min_num, k)
-                
-        return curr_min_num if curr_min_num != float("inf") else -1
+        mx = -1
+        smallest = float("inf")
+
+        for k, v in even.items():
+            if v > mx:
+                mx = v
+                smallest = k
+            elif v == mx:
+                smallest = min(smallest, k)
+        
+        return smallest if smallest != float("inf") else -1
