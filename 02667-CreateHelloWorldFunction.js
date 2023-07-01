b4c36f077bd85a8b821ed48f245b4781f0990cce
Han-Elliot Nguyen <hanelliotn@gmail.com>
2023-06-30 21:51:08 -0700

    Create 02667-CreateHelloWorldFunction.js

---
+++ 02667-CreateHelloWorldFunction.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/create-hello-world-function/description/
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return () => "Hello World";
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
