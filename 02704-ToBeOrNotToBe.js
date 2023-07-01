3ef166f5ef408e7d3a47631c3f9b764914191355
Han-Elliot Nguyen <hanelliotn@gmail.com>
2023-06-30 22:00:47 -0700

    Create 02704-ToBeOrNotToBe.js

---
+++ 02704-ToBeOrNotToBe.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/to-be-or-not-to-be/description/
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (v) => {
+            if(val !== v) throw new Error("Not Equal");
+            return true;
+        },
+        notToBe: (v) => {
+            if(val === v) throw new Error("Equal");
+            return true;
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
