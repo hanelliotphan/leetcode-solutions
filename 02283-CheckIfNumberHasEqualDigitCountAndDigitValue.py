7c2cb5f350530cfcb187a1d2ca97600fc7e5af8d
Han-Elliot Nguyen <hanelliotn@gmail.com>
2023-07-04 20:23:32 -0700

    Update 02283-CheckIfNumberHasEqualDigitCountAndDigitValue.py

--- 02283-CheckIfNumberHasEqualDigitCountAndDigitValue.py
+++ 02283-CheckIfNumberHasEqualDigitCountAndDigitValue.py
@@ -1,25 +1,17 @@
+# https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/description/
+
 class Solution:
     def digitCount(self, num: str) -> bool:
         """
-        Logic: Hash Map of Indices & Frequencies
-        
+        Logic: Hash Map/Counter
+
         Time: O(n)
         Space: O(n)
         """
-        counter = dict()
-        
-        for n in num:
-            if int(n) not in counter:
-                counter[int(n)] = 1
-            else:
-                counter[int(n)] += 1
-                
+        counter = collections.Counter(num)
+
         for i in range(len(num)):
-            if i not in counter:
-                counter[i] = 0
-                
-        for i in range(len(num)):
-            if i in counter and counter[i] != int(num[i]):
+            if counter[str(i)] != int(num[i]):
                 return False
-            
-        return True
+        
+        return True 
