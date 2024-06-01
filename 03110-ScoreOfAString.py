6dbd27288630f6dcd5be33f595609081afb0dd4a
Han-Elliot Phan <hanelliotphan@gmail.com>
2024-05-31 20:50:32 -0700

    Create 03110-ScoreOfAString.py

---
+++ 03110-ScoreOfAString.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/score-of-a-string/description/
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+
+        for i in range(1, len(s)):
+            score += abs(ord(s[i])-ord(s[i-1]))
+        
+        return score
