9917d20424a64a18a48145ec7e1df86370bb7cf1
Han-Elliot Phan <hanelliotphan@gmail.com>
2024-09-17 16:48:48 -0700

    Create 03150-InvalidTweets2.sql

---
+++ 03150-InvalidTweets2.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT tweet_id FROM Tweets 
+WHERE LENGTH(content) > 140 OR
+    LENGTH(content) - LENGTH(REPLACE(content, '@', '')) > 3 OR
+    LENGTH(content) - LENGTH(REPLACE(content, '#', '')) > 3
